01cc1a9abc0ac484b5a015ec927f1bdd6b77e6ac
Arthur <arthurki2047@gmail.com>
2025-12-31 20:12:22 +0000

    https://d3eyhgoylams0m.cloudfront.net/v1/manifest/93ce20f0f52760bf38be91

--- src/lib/channels.ts
+++ src/lib/channels.ts
@@ -11,7 +11,7 @@ export interface Channel {
 
 const channelData = [
   { id: '3', name: 'Colors Bangla', category: 'Entertainment', imageId: 'tv16' },
-  { id: '4', name: 'Cartoon World', category: 'Kids', imageId: 'tv4' },
+  { id: '4', name: 'DD Bangla', category: 'Entertainment', imageId: 'tv17' },
   { id: '5', name: 'Discovery Earth', category: 'Documentary', imageId: 'tv5' },
   { id: '6', name: 'Music Hits', category: 'Music', imageId: 'tv6' },
   { id: '7', name: 'ABP Ananda', category: 'News', imageId: 'tv15' },
@@ -36,6 +36,8 @@ export const channels: Channel[] = channelData.map(channel => {
     streamUrl = 'https://d3qs3d2rkhfqrt.cloudfront.net/out/v1/ceda14583477426aa162a65392d8ea07/index.m3u8';
   } else if (channel.id === '7') {
     streamUrl = 'https://d35j504z0x2vu2.cloudfront.net/v1/master/0bc8e8376bd8417a1b6761138aa41c26c7309312/abp-ananda/master.m3u8';
+  } else if (channel.id === '4') {
+    streamUrl = 'https://d3eyhgoylams0m.cloudfront.net/v1/manifest/93ce20f0f52760bf38be911ff4c91ed02aa2fd92/ed7bd2c7-8d10-4051-b397-2f6b90f99acb/2e9e32a4-c4f7-49c3-96d6-c4e3660c7e3f/2.m3u8';
   } else {
     const streamId = channel.id === '54161' ? '54161' : channel.id;
     streamUrl = `https://allinonereborn.xyz/amit/host.php?id=${streamId}`;
